72c96a8356818a8cd8e2d4946ef1091eb67fa68c
Ben Harrison <bmhtech07@users.noreply.github.com>
2022-01-25 17:30:21 +0000

    Add key to template for Vue 3

--- components/shareIt.vue
+++ components/shareIt.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="vue-share-it-wrapper">
-    <template v-for="(config, index) in finalConfig">
+    <template v-for="(config, index) in finalConfig" :key="`share-target-${index}`">
       <a
-        :key="`share-target-${index}`"
         href="javascript:void(0)"
         @click="openPopUp(config)"
         :style="{ 
